f470dff1cfbb81dc2989d364592bb0d1ae90cc02
Besghaier <besghaier.mohamed.04@gmail.com>
2026-04-01 16:00:19 +0200

    66 plus one

---
+++ math/66-plus-one.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+
+        for (int i = digits.size()-1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
+
+// Test
+
+int main()
+{
+    Solution sol;
+    vector<int> digits = {9, 9, 9};
+    vector<int> result = sol.plusOne(digits);
+
+    for (int x : result) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
